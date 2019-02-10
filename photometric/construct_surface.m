6734a7054628ad39ee34d68e517f2c9688550237
Victor <victorzuanazzi@gmail.com>
2019-02-10 11:19:30 +0100

    1.3 construct surface working

--- photometric/construct_surface.m
+++ photometric/construct_surface.m
@@ -6,47 +6,49 @@ function [ height_map ] = construct_surface( p, q, path_type )
 %   'row', or 'average'
 %   height_map: the reconstructed surface
 
-
 if nargin == 2
     path_type = 'column';
 end
 
+p = double(p);
+q = double(q);
+
 [h, w] = size(p);
-height_map = zeros(h, w);
+height_map = zeros(h, w, 'double');
 
 switch path_type
     case 'column'
-        % =================================================================
-        % YOUR CODE GOES HERE
         % top left corner of height_map is zero
         % for each pixel in the left column of height_map
         %   height_value = previous_height_value + corresponding_q_value
+        height_map(2:h,1) = height_map(1:h-1,1) + q(2:h,1);
         
         % for each row
         %   for each element of the row except for leftmost
-        %       height_value = previous_height_value + corresponding_p_value
-        
-
-       
-        % =================================================================
-               
+        %       height_value = previous_height_value + corresponding_p_value      
+        height_map(:,2:w) = height_map(:,1:w-1) + p(:,2:w);
+                    
     case 'row'
+        % top left corner of height_map is zero
+        % for each pixel in the top row of height_map
+        %   height_value = previous_height_value + corresponding_p_value
+        height_map(1,2:w) = height_map(1,1:w-1) + p(1,2:w);
         
-        % =================================================================
-        % YOUR CODE GOES HERE
+        % for each columns
+        %   for each element of the columns except for top one
+        %       height_value = previous_height_value + corresponding_q_value      
+        height_map(2:h,:) = height_map(1:h-1,:) + q(2:h,:);
         
-
-        % =================================================================
-          
     case 'average'
+        %height_map by row
+        row_height_map = construct_surface( p, q, 'row' );
+        %height_map by row
+        column_height_map = construct_surface( p, q, 'column' );
         
-        % =================================================================
-        % YOUR CODE GOES HERE
-
+        %average of the two
+        height_map = (row_height_map + column_height_map) / 2;
         
-        % =================================================================
 end
-
 
 end
 
