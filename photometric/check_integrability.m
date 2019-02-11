80c06e6cfac6e618aa07288700e3337f07ebcd5a
Victor <victorzuanazzi@gmail.com>
2019-02-11 20:45:33 +0100

    fixed 2nd derivatives

--- photometric/check_integrability.m
+++ photometric/check_integrability.m
@@ -29,9 +29,17 @@ q(isnan(q)) = 0;
 SE = zeros([h,w]);
 dpdy = zeros([h,w]);
 dqdx = zeros([h,w]);
-dpdy(:,2:end) = p(:,1:end-1) - p(:,2:end);
-dqdx(2:end,:) = q(1:end-1,:) - q(2:end,:);
-SE = (dpdy - dqdx)^2;
+
+p_y1 = p(:,1);
+q_x1 = q(1,:);
+
+%approximate derivatives
+dpdy = diff([p_y1 p], 1, 2); 
+dqdx = diff([q_x1; q], 1, 1); 
+
+%dpdy(:,2:end) = p(:,1:end-1) - p(:,2:end); 
+%dqdx(2:end,:) = q(1:end-1,:) - q(2:end,:);
+SE = (dpdy - dqdx).^2;
 % ========================================================================
 end
 
