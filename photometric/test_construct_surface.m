1796494a4d1b6f30774fc29f3cd779e0a935c72b
Victor <victorzuanazzi@gmail.com>
2019-02-11 21:54:36 +0100

    test function for contruct surface

---
+++ photometric/test_construct_surface.m
@@ -0,0 +1,69 @@
+close all
+clear all
+clc
+ 
+disp('Part 1: Photometric Stereo')
+
+% obtain many images in a fixed view under different illumination
+disp('Loading images...')
+image_dir = './photometrics_images/SphereGray5/';
+%image_ext = '*.png';
+
+[image_stack, scriptV] = load_syn_images(image_dir);
+[h, w, n] = size(image_stack);
+fprintf('Finish loading %d images.\n\n', n);
+
+% compute the surface gradient from the stack of imgs and light source mat
+disp('Computing surface albedo and normal map...')
+[albedo, normals] = estimate_alb_nrm(image_stack, scriptV);
+
+
+%% integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
+disp('Integrability checking')
+[p, q, SE] = check_integrability(normals);
+
+threshold = 0.005;
+SE(SE <= threshold) = NaN; % for good visualization
+fprintf('Number of outliers: %d\n\n', sum(sum(SE > threshold)));
+
+%% compute the surface height
+height_map_row = construct_surface( p, q, 'row' );
+height_map_col = construct_surface( p, q, 'column' );
+height_map_ave = construct_surface( p, q, 'average' );
+height_map_har = construct_surface( p, q, 'm_average');
+
+
+%% Display
+show_results(albedo, normals, SE);
+show_model(albedo, height_map_row);
+show_model(albedo, height_map_col);
+show_model(albedo, height_map_ave);
+show_model(albedo, height_map_har);
+
+%% Face
+[image_stack, scriptV] = load_face_images('./photometrics_images/yaleB02/');
+[h, w, n] = size(image_stack);
+fprintf('Finish loading %d images.\n\n', n);
+disp('Computing surface albedo and normal map...')
+[albedo, normals] = estimate_alb_nrm(image_stack, scriptV);
+
+%% integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
+disp('Integrability checking')
+[p, q, SE] = check_integrability(normals);
+
+threshold = 0.005;
+SE(SE <= threshold) = NaN; % for good visualization
+fprintf('Number of outliers: %d\n\n', sum(sum(SE > threshold)));
+
+%% compute the surface height
+height_map_row = construct_surface( p, q, 'row' );
+height_map_col = construct_surface( p, q, 'column' );
+height_map_ave = construct_surface( p, q, 'average' );
+height_map_har = construct_surface( p, q, 'm_average' );
+
+show_results(albedo, normals, SE);
+show_model(albedo, height_map_row);
+show_model(albedo, height_map_col);
+show_model(albedo, height_map_ave);
+show_model(albedo, height_map_har);
+
