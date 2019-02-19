db15f78ed06cee4e0950fe36168acc06027da219
Victor <victorzuanazzi@gmail.com>
2019-02-19 11:16:29 +0100

    function to show the normal map

---
+++ photometric/display_normal_vectors.m
@@ -0,0 +1,12 @@
+function [] = display_normal_vectors(height_map, p, q)
+%surf(height_map);\
+    
+s = 9;
+[w, h, ~] = size(height_map);
+
+[X, Y] = meshgrid(1:s:h, 1:s:w);
+figure();
+[U, V, W] = surfnorm(X, Y, height_map(1:s:end, 1:s:end));
+quiver3(X, Y, height_map(1:s:end, 1:s:end), q(1:s:end, 1:s:end), p(1:s:end, 1:s:end), W, 0.5);
+
+end
